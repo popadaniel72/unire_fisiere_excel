8ea154e072cacd4308dff784bb519cde3e5385e0
popadaniel72 <popadaniel72@yahoo.com>
2021-07-27 22:32:33 +0300

    Add project files.

---
+++ unire_fisiere_excel/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace unire_fisiere_excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numeDirector = new System.Windows.Forms.Label();
+            this.od1 = new System.Windows.Forms.OpenFileDialog();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cauta fisiere";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numeDirector
+            // 
+            this.numeDirector.AutoSize = true;
+            this.numeDirector.Location = new System.Drawing.Point(12, 47);
+            this.numeDirector.Name = "numeDirector";
+            this.numeDirector.Size = new System.Drawing.Size(35, 13);
+            this.numeDirector.TabIndex = 1;
+            this.numeDirector.Text = "label1";
+            // 
+            // od1
+            // 
+            this.od1.FileName = "openFileDialog1";
+            this.od1.Filter = "Fisier Excel|*.xlsx";
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(6, 19);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(135, 303);
+            this.lista.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lista);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 340);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lista de fisiere";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numeDirector);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Unire fisiere Excel";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label numeDirector;
+        private System.Windows.Forms.OpenFileDialog od1;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ unire_fisiere_excel/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace unire_fisiere_excel
+{
+    public partial class Form1 : Form
+    {
+        int ok;
+        Excel.Application ex;
+        Excel.Workbook doc, docUnite;
+        Excel.Worksheet pag, pagUniteP1, pagUniteP2, pagUniteP3, pagUniteM;
+        int pu1, pu2, pu3, pum;
+        public Form1()
+        {
+            InitializeComponent();
+            ok = 0;
+            ex = new Excel.Application();
+            docUnite = ex.Workbooks.Add();
+            docUnite.Worksheets.Add();
+            docUnite.Worksheets.Add();
+            docUnite.Worksheets.Add();
+            pagUniteP1 = (Excel.Worksheet)docUnite.Worksheets.get_Item(1);
+            pagUniteP2 = (Excel.Worksheet)docUnite.Worksheets.get_Item(2);
+            pagUniteP3 = (Excel.Worksheet)docUnite.Worksheets.get_Item(3);
+            pagUniteM = (Excel.Worksheet)docUnite.Worksheets.get_Item(4);
+            pagUniteP1.Name = "Premiul 1";
+            pagUniteP2.Name = "Premiul 2";
+            pagUniteP3.Name = "Premiul 3";
+            pagUniteM.Name = "Mentiune";
+            pagUniteP1.Cells[1, 1] = "Nume";
+            pagUniteP1.Cells[1, 2] = "Clasa";
+            pagUniteP1.Cells[1, 3] = "Media";
+            pu1 = pu2 = pu3 = pum = 1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            od1.InitialDirectory = Directory.GetCurrentDirectory();
+            if(od1.ShowDialog() == DialogResult.OK)
+            {
+                numeDirector.Text = Path.GetDirectoryName( od1.FileName);
+                lista.Items.Clear();
+                string[] l = Directory.GetFiles(numeDirector.Text);
+                for(int i = 0; i< l.Length; i++)
+                {
+                    if(Path.GetExtension( l[i])==".xlsx")
+                    {
+                        lista.Items.Add(Path.GetFileNameWithoutExtension(l[i]));
+                        doc = ex.Workbooks.Open(l[i]);
+                        pag = doc.Sheets[1];
+                        for(int j = 2;pag.Cells[j,3].Value2!=null; j++)
+                        {
+                            if(pag.Cells[j, 3].Value2=="I")
+                            {
+                                pu1++;
+                                pagUniteP1.Cells[pu1, 1] = pag.Cells[j, 1];
+                                pagUniteP1.Cells[pu1, 2] = Path.GetFileNameWithoutExtension(l[i]);
+                                pagUniteP1.Cells[pu1, 3] = pag.Cells[j, 2];
+                            }
+                        }
+                    }
+                }
+
+                /// salvare finala
+                if (File.Exists(numeDirector.Text + @"\Unite.xlsx"))
+                    File.Delete(numeDirector.Text + @"\Unite.xlsx");
+                
+                docUnite.SaveAs(numeDirector.Text + @"\Unite.xlsx");
+                docUnite.Close();
+                ex.Quit();
+            }
+        }
+    }
+}
